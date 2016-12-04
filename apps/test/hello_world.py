dd55f57b7f8786ef7c4eda76dc0cf14cea65cbbe
sciooga <sciooga@gmail.com>
2016-12-04 16:12:34 +0800

    add self.logger and fixed some bug

--- apps/test/hello_world.py
+++ apps/test/hello_world.py
@@ -10,10 +10,10 @@ class HelloWorld(BaseHandler):
         """
 
         self.rd.set('123','321')
-        print(self.rd.get('123'))
+        self.logger.info(self.rd.get('123'))
 
         self.db.Test.insert({'test': 'test'})
-        print self.db.Test.find_one({'test': 'test'})
+        self.logger.info(self.db.Test.find_one({'test': 'test'}))
 
         return self.write_json({
             'errcode': 0,
--- settings.py
+++ settings.py
@@ -4,3 +4,6 @@ PROJECT_NAME = 'qianai'
 RD_HOST = 'localhost'
 RD_PORT = 6379
 RD_DB = 0
+
+# mongo
+MONGO_URI = 'localhost:27017'
